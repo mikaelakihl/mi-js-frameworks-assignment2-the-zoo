12e82ceba63329ad8098a449923a5c74a6ab144d
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-25 14:00:30 +0200

    feat: added some styling to layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,23 +2,24 @@ import { NavLink, Outlet } from "react-router"
 
 export const Layout = () => {
     return (
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to={'/'}>Hem</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/animals'}>Djuren</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
+        <>  <div className="flex flex-col h-screen">
+                <header className="bg-yellow-400 flex items-center justify-center">
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to={'/'}>Hem</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/animals'}>Djuren</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+            <main className="flex-1 bg-red-200">
                 <Outlet/>
             </main>
-            <footer></footer>
+            <footer className="h-[10vh] bg-green-700 flex items-center justify-center text-white"></footer>
+            </div>
         </>
     )
 }
