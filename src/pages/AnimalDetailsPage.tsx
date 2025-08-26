5015da9aec2fb97ffa699c7918a8b53f7a890a45
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-26 14:52:00 +0200

    feat: used context to make "feed" button

--- src/pages/AnimalDetailsPage.tsx
+++ src/pages/AnimalDetailsPage.tsx
@@ -13,12 +13,21 @@
 import { useLocation, Navigate, Link } from "react-router-dom";
 import type { Animal } from "../models/Animal";
 import { AnimalHero } from "../components/AnimalHero";
+import { useAnimals } from "../context/AnimalsContext";
+
+
 
 type LocationState = { animal?: Animal };
 
 export const AnimalDetailsPage = () => {
+  
   const { state } = useLocation() as { state?: LocationState };
   const animal = state?.animal;
+  
+  const {feed, getStatus} = useAnimals();
+
+  // const isFed = canFeed(animal.id);
+  const status = getStatus(animal.id);
 
   if (!animal) return <Navigate to="/animals" replace />;
 
@@ -29,6 +38,8 @@ export const AnimalDetailsPage = () => {
       </div>
       <h2>Animaldetails</h2>
       <AnimalHero animal={animal} className=" h-70 md:[&_img]:w-[280px] md:[&_img]:h-[280px] md:[&_h3]:w-[280px] md:[&_h3]:h-[50px] md:[&_h3]:bottom-[0px] " />
+      <button className="px-4 py-2 rounded-2xl shadow hover:shadow-md active:scale-95 transition" onClick={()=>feed(animal.id)}>Mata</button>
+      <div>{status}</div>
       
     </section>
   );
