c805bd75ddc97de954dc7298a1e2c1400392c096
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-29 13:01:56 +0200

    refactor: removed unnecessary imports

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router";
-import { AnimalHero } from "../components/AnimalHero";
 import { useAnimals } from "../context/AnimalsContext"
 
 export const HomePage = () => {
