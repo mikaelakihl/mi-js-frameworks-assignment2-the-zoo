b67eb3150f988f3af60cd47bad9786ab27e01575
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-29 10:58:03 +0200

    feat: changed name of Zoo and deleted FeedAnimal component

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,8 +5,8 @@ export const Layout = () => {
         
         <> 
             <div className="flex flex-col min-h-screen">
-                <header className="min-h-20 overflow-hidden bg-yellow-400 flex justify-between border-b border-black p-2 items-center">
-                    <h2>Zoo</h2>
+                <header className="min-h-20 overflow-hidden bg-yellow-400 flex justify-between p-2 items-center">
+                    <h2 className="font-oswald">Zafarie</h2>
                     <nav>
                         <ul className="flex flex-row gap-2">
                             <li>
@@ -24,7 +24,7 @@ export const Layout = () => {
             </main>
             <footer className="h-[20vh] bg-green-700 flex items-center justify-center text-white ">
                 <div>
-                    <p>© Mikaela Kihl | Djur på Zoo</p>
+                    <p>© Mikaela Kihl | Zafari</p>
                 </div>
             </footer>
             </div>
